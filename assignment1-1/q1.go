468f3d58f570a889c9d445207cd267534fd000a5
Rain <hanyuking2016@gmail.com>
2023-03-14 16:05:42 +0800

    q1

--- assignment1-1/q1.go
+++ assignment1-1/q1.go
@@ -1,8 +1,12 @@
 package cos418_hw1_1
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"regexp"
 	"sort"
+	"strings"
 )
 
 // Find the top K most common words in a text document.
@@ -18,7 +22,62 @@ func topWords(path string, numWords int, charThreshold int) []WordCount {
 	// TODO: implement me
 	// HINT: You may find the `strings.Fields` and `strings.ToLower` functions helpful
 	// HINT: To keep only alphanumeric characters, use the regex "[^0-9a-zA-Z]+"
-	return nil
+
+	wordMap := make(map[string]int)
+	regex, err := regexp.Compile("[^0-9a-zA-Z]+")
+	checkError(err)
+
+	readFile(path, func(lineText string) {
+		words := strings.Fields(lineText)
+		for _, word := range words {
+
+			if len(word) == 0 || len(word) < charThreshold {
+				continue
+			}
+
+			word = strings.ToLower(word)
+
+			if regex.Match([]byte(word)) {
+				var newWord string
+				for i := 0; i < len(word); i++ {
+					c := word[i]
+					if c >= '0' && c <= '9' || c >= 'a' && c <= 'z' {
+						newWord += fmt.Sprintf("%c", c)
+					}
+				}
+				word = newWord
+			}
+
+			wordMap[word] += 1
+		}
+	})
+
+	var wordCounts []WordCount
+	for key, value := range wordMap {
+		wordCounts = append(wordCounts, WordCount{
+			Word:  key,
+			Count: value,
+		})
+	}
+	sortWordCounts(wordCounts)
+	return wordCounts[0:numWords]
+}
+
+func readFile(path string, lineReadCallback func(lineText string)) {
+	// open file
+	f, err := os.Open(path)
+	checkError(err)
+	// remember to close the file at the end of the program
+	defer f.Close()
+
+	// read the file line by line using scanner
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		lineReadCallback(scanner.Text())
+	}
+
+	checkError(scanner.Err())
 }
 
 // A struct that represents how many times a word is observed in a document
