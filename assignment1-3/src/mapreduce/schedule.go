911a74f323f1b4ff23cecc09433bf9b27f45d78f
Rain <hanyuking2016@gmail.com>
2023-12-08 17:13:29 +0800

    assignment1-3: TestBasic PASS

--- assignment1-3/src/mapreduce/schedule.go
+++ assignment1-3/src/mapreduce/schedule.go
@@ -1,5 +1,7 @@
 package mapreduce
 
+import "sync"
+
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
 	var ntasks int
@@ -22,5 +24,33 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < ntasks; i++ {
+		wg.Add(1)
+
+		go func(i int) {
+			for {
+				worker := <-mr.registerChannel
+				args := DoTaskArgs{
+					JobName:       mr.jobName,
+					File:          mr.files[i],
+					Phase:         phase,
+					TaskNumber:    i,
+					NumOtherPhase: nios,
+				}
+				success := call(worker, "Worker.DoTask", args, &ShutdownReply{})
+				if success {
+					wg.Done()
+					mr.registerChannel <- worker
+					break
+				}
+			}
+		}(i)
+	}
+
+	wg.Wait()
+
 	debug("Schedule: %v phase done\n", phase)
 }
